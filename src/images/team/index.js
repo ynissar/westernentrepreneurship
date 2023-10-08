9b56bb5e0fdd3307d4240ee2a0df6d3a3802843c
stephan <stephanbalakumaran1@gmail.com>
2023-10-08 15:45:32 -0400

    updates 1.0

--- src/images/team/index.js
+++ src/images/team/index.js
@@ -1,29 +1,36 @@
-import Salina from "./salinaGuo.jpg";
-import Nathan from "./nathanYee.jpg";
+import Salina from "./salina.jpg";
+import Abhi from "./abhi.jpg";
 import Sandy from "./sandyWu.jpeg";
 import Zohaib from "./zohaibAdnan.png";
 import Stephan from "./stephanBalakumaran.jpg";
-import Leanne from "./leanneKwong.jpg";
-import Sunny from "./sunnySu.png";
-import Mohammed from "./mohammedKarimjee.jpg";
-import Grace from "./graceZhou.jpg";
-import Lena from "./lenaQiu.jpg";
-import Kelly from "./kellyLe.jpg";
-import Yusuf from "./yusufNissar.png";
-import Orianna from "./oriannaLui.jpg";
+import Amy from "./amy.jpg";
+import Annie from "./annie.jpg";
+import Carina from "./carina.jpg";
+import Cheryl from "./cheryl.jpg";
+import Melanie from "./melanie.jpg";
+import Tristan from "./tristan.jpg";
+import Uttej from "./uttej.png";
+import Michael from "./michael.jpg";
+import Michelle from "./michelle.jpg";
+import Peyton from "./peyton.jpg";
+import Daniel from "./daniel.jpg";
+
 
 export {
   Salina,
-  Nathan,
+  Abhi,
   Sandy,
   Zohaib,
   Stephan,
-  Leanne,
-  Sunny,
-  Mohammed,
-  Lena,
-  Grace,
-  Kelly,
-  Yusuf,
-  Orianna,
+  Amy,
+  Annie,
+  Carina,
+  Cheryl,
+  Melanie,
+  Tristan,
+  Uttej,
+  Michael,
+  Michelle,
+  Peyton,
+  Daniel,
 };
